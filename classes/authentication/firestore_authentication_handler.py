fadd109774513f307f374bcf85e202b8765c7019
Scot Curry <scotcurry4@gmail.com>
2023-09-19 11:15:19 -0400

    Updated firestore_authentication_handler.py.

--- classes/authentication/firestore_authentication_handler.py
+++ classes/authentication/firestore_authentication_handler.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import sys
 import logging
 
@@ -7,6 +8,8 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 from classes.settings.app_settings_handler import load_app_settings, get_file_path
+from classes.authentication.google_build_authentication import \
+    build_json_from_environment_variables
 
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler(sys.stdout)
@@ -17,11 +20,23 @@ logger.setLevel(level=LOGLEVEL)
 
 def get_firestore_database():
 
-    app_settings = load_app_settings()
-    authentication_file = app_settings['settings']['firebase_authentication_file']
-    authentication_file_path = get_file_path(authentication_file)
+    build_json_from_environment_variables()
+    os_type = platform.system()
+    if os_type == 'Darwin':
+        authentication_file_path = os.environ['TMPDIR'] + 'credentials.json'
+    else:
+        temp_folder = os.environ['RUNNER_TEMP']
+        if not temp_folder.endswith('/'):
+            temp_folder = temp_folder + '/'
+
+        authentication_file_path = temp_folder + 'credentials.json'
+
+    # app_settings = load_app_settings()
+    # authentication_file = app_settings['settings']['firebase_authentication_file']
+    # authentication_file_path = get_file_path(authentication_file)
 
     creds = credentials.Certificate(authentication_file_path)
+    os.remove(authentication_file_path)
     try:
         firebase_admin.get_app('currywareff')
     except ValueError:
